8de92d0274ea0ed1953c7777e1a3a2929f6ef7c1
AndresJG <andres.jimenez3@utp.edu.co>
2023-12-19 18:31:39 -0500

    refactor(theme): Changing theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,27 +3,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
-    // colors: {
-    //   // 'blue': '#1fb6ff',
-    //   // 'purple': '#7e5bef',
-    //   // 'pink': '#ff49db',
-    //   // 'orange': '#ff7849',
-    //   // 'green': '#13ce66',
-    //   // 'yellow': '#ffc82c',
-    //   // 'gray-dark': '#273444',
-    //   // 'gray': '#8492a6',
-    //   // 'gray-light': '#d3dce6',
-    // },
-    // colors: theme => ({
-    //   red: '#ff0000',
-    //   green: '#00ff00',
-    //   blue: {
-    //     light: '#0000f0',
-    //     DEFAULT: '#0000fa',
-    //     dark: '#0000ff',
-    //   },
-    // }),
+    extend: {
+      colors: {
+        green: {
+          DEFAULT: '#8492a6',
+          600: '#02885F',
+          700: '#014C3F'
+        },
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
